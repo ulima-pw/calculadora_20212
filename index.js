1ca732f7179893892b2408217853e47d28f6edcc
Hernan <hernan@jsatch.com>
2021-09-13 19:52:39 -0500

    onclick de primeros botones

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function clickMeOnClick() {
-    console.log("Hola")
+/*function numeroOnClick(numero) {
+    const inputNumeros = document.getElementById("txt_numeros")
+    inputNumeros.value = numero
+}*/
+
+const numeroOnClick = (numero) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    inputNumeros.value = numero
 }

c2030c699944872743b780bae1523f1046d8b305
Hernan <hernan@jsatch.com>
2021-09-15 17:54:41 -0500

    Operacions principales terminadas

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-/*function numeroOnClick(numero) {
-    const inputNumeros = document.getElementById("txt_numeros")
-    inputNumeros.value = numero
-}*/
+var numeroIngresado;
+var enOperacion = false;
+var operacionActual;
+
+const sumar = (sum1, sum2) => {
+    return sum1 + sum2;
+}
+
+const restar = (n1, n2) => {
+    return n1 - n2;
+}
 
 const numeroOnClick = (numero) => {
     const inputNumeros = document.getElementById("txt_numeros")
-    inputNumeros.value = numero
+    const numeroStr = inputNumeros.value;
+    if (numeroStr == "0" || enOperacion) {
+        inputNumeros.value = numero;
+        enOperacion = false;
+    }else {
+        inputNumeros.value = numeroStr + numero;
+    }
+}
+
+const operacionOnClick = (operacion) => {
+    const inputNumeros = document.getElementById("txt_numeros")
+    if (operacion == "C") {
+        inputNumeros.value = "0";
+    }else if (operacion == "+") {
+        const numero = inputNumeros.value
+        numeroIngresado = parseInt(numero)
+        enOperacion = true;
+        operacionActual = sumar;
+    }else if (operacion == "-") {
+        const numero = inputNumeros.value
+        numeroIngresado = parseInt(numero)
+        enOperacion = true;
+        operacionActual = restar;
+    }else if (operacion == "=") {
+        if (operacionActual != undefined) {
+            const numero = inputNumeros.value
+            const res = operacionActual(numeroIngresado, parseInt(numero))
+            inputNumeros.value = res;
+        }
+    }
 }

7ef0d97437814f96a9536f1ec294d262f504ab3d
Hernan <hernan@jsatch.com>
2021-09-20 19:17:01 -0500

    armando layout por js

--- index.js
+++ index.js
@@ -56,18 +56,96 @@ const operacionOnClickNoObstrusivo = (evt) => {
     operacionOnClick(op)
 }
 
+const crearInputNumeros = () => {
+    var inputNumeros = document.createElement("input")
+    inputNumeros.setAttribute("id", "txt_numeros")
+    inputNumeros.setAttribute("class", "form-control mb-3")
+    inputNumeros.setAttribute("type", "text")
+    inputNumeros.setAttribute("value", "0")
+
+    return inputNumeros
+}
+
+const crearDivFilaBotones = (numFila) => {
+    var div = document.createElement("div")
+    div.setAttribute("class", "row mb-1")
+
+
+    for (var i = 0; i< 4; i++) {
+        var divCol = document.createElement("div")
+        divCol.setAttribute("class", "col")
+
+        var etiqueta = ""
+        if (i == 3) {
+            // Boton operacion
+            switch(numFila) {
+                case 7: 
+                    etiqueta= "+"
+                break;
+                case 4: etiqueta= "-"
+                break;
+                case 1: etiqueta= "*"
+                break;
+            }
+            
+        }else {
+            etiqueta = (numFila + i)
+        }
+    
+        var but = document.createElement("button")
+        but.setAttribute("id", "but" + etiqueta)
+        but.setAttribute("class", "btn btn-primary")
+        but.setAttribute("type", "button")
+        but.innerText = etiqueta
+        divCol.appendChild(but)
+        div.appendChild(divCol)
+    }
+    return div;
+}
+
+const crearUltimaFila = () => {
+    var div = document.createElement("div")
+    div.setAttribute("class", "row mb-1")
+
+    var divCol = document.createElement("div")
+    divCol.setAttribute("class", "col")
+}
+
+const construirCalculadora = () => {
+    var inputNumeros = crearInputNumeros();
+    document.body.appendChild(inputNumeros);
+
+    var divFila = crearDivFilaBotones(7)
+    document.body.appendChild(divFila);
+
+    divFila = crearDivFilaBotones(4)
+    document.body.appendChild(divFila);
+
+    divFila = crearDivFilaBotones(1)
+    document.body.appendChild(divFila);
+
+    divFila = crearUltimaFila()
+}
+
+
+
 const main = () => {
+    construirCalculadora();
+
+
 
     for (var i =0 ; i < 10; i++) {
         const but = document.getElementById("but" + i)
-        but.onclick = numeroOnClickNoObstrusivo
+        //but.onclick = numeroOnClickNoObstrusivo
+        but.addEventListener("click", numeroOnClickNoObstrusivo)
     }
 
     const arrOperaciones = ["+", "-", "C", "="]
     for (var op of arrOperaciones) {
         const but = document.getElementById("but" + op)
-        but.onclick = operacionOnClickNoObstrusivo
+        but.setAttribute("billy", "pw")
+        but.onclick = operacionOnClickNoObstrusivo;
     }
 }
 
-main();
+window.addEventListener("load", main);
